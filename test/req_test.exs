c3b8baa917039937d5e61236dc3d5fa9afa47926
Wojtek Mach <wojtek@wojtekmach.pl>
2021-04-04 11:06:54 +0200

    Add mint example test

--- test/req_test.exs
+++ test/req_test.exs
@@ -393,6 +393,67 @@ defmodule ReqTest do
     refute_received _
   end
 
+  test "mint", c do
+    Bypass.expect(c.bypass, "GET", "/json", fn conn ->
+      conn
+      |> Plug.Conn.put_resp_content_type("application/json")
+      |> Plug.Conn.send_resp(200, Jason.encode_to_iodata!(%{"a" => 1}))
+    end)
+
+    response =
+      Req.build(:get, c.url <> "/json")
+      |> Req.add_default_steps()
+      |> Req.add_request_steps([
+        &mint/1
+      ])
+      |> Req.run!()
+
+    assert response.status == 200
+    assert response.body == %{"a" => 1}
+  end
+
+  defp mint(request) do
+    scheme =
+      case request.uri.scheme do
+        "https" -> :https
+        "http" -> :http
+      end
+
+    {:ok, conn} = Mint.HTTP.connect(scheme, request.uri.host, request.uri.port)
+
+    {:ok, conn, request_ref} =
+      Mint.HTTP.request(conn, "GET", request.uri.path, request.headers, request.body)
+
+    response = mint_recv(conn, request_ref, %{})
+    {request, response}
+  end
+
+  defp mint_recv(conn, request_ref, acc) do
+    receive do
+      message ->
+        {:ok, conn, responses} = Mint.HTTP.stream(conn, message)
+        acc = Enum.reduce(responses, acc, &mint_process(&1, request_ref, &2))
+
+        if acc[:done] do
+          Map.delete(acc, :done)
+        else
+          mint_recv(conn, request_ref, acc)
+        end
+    end
+  end
+
+  defp mint_process({:status, request_ref, status}, request_ref, response),
+    do: put_in(response[:status], status)
+
+  defp mint_process({:headers, request_ref, headers}, request_ref, response),
+    do: put_in(response[:headers], headers)
+
+  defp mint_process({:data, request_ref, new_data}, request_ref, response),
+    do: update_in(response[:body], fn data -> (data || "") <> new_data end)
+
+  defp mint_process({:done, request_ref}, request_ref, response),
+    do: put_in(response[:done], true)
+
   ## Helpers
 
   defp unreachable(_request) do
