ef94d867a68ffb5d5c2918726cb393a2def9c9cf
Wojtek Mach <wojtek@wojtekmach.pl>
2022-04-28 11:43:41 +0200

    Update tests

--- test/req_test.exs
+++ test/req_test.exs
@@ -11,43 +11,41 @@ defmodule ReqTest do
     [bypass: bypass, url: "http://localhost:#{bypass.port}"]
   end
 
-  describe "high-level API" do
-    test "get!/2", c do
-      Bypass.expect(c.bypass, "GET", "/", fn conn ->
-        conn
-        |> Plug.Conn.put_resp_content_type("application/json")
-        |> Plug.Conn.send_resp(200, Jason.encode_to_iodata!(%{"a" => 1}))
-      end)
+  test "get!/2", c do
+    Bypass.expect(c.bypass, "GET", "/", fn conn ->
+      conn
+      |> Plug.Conn.put_resp_content_type("application/json")
+      |> Plug.Conn.send_resp(200, Jason.encode_to_iodata!(%{"a" => 1}))
+    end)
 
-      assert Req.get!(c.url).body == %{"a" => 1}
-    end
+    assert Req.get!(c.url).body == %{"a" => 1}
+  end
 
-    test "post!/2", c do
-      Bypass.expect(c.bypass, "POST", "/", fn conn ->
-        conn
-        |> Plug.Conn.put_resp_content_type("application/json")
-        |> Plug.Conn.send_resp(200, Jason.encode_to_iodata!(%{"a" => 1}))
-      end)
+  test "post!/2", c do
+    Bypass.expect(c.bypass, "POST", "/", fn conn ->
+      conn
+      |> Plug.Conn.put_resp_content_type("application/json")
+      |> Plug.Conn.send_resp(200, Jason.encode_to_iodata!(%{"a" => 1}))
+    end)
 
-      assert Req.post!(c.url, json: %{foo: "bar"}).body == %{"a" => 1}
-    end
+    assert Req.post!(c.url, json: %{foo: "bar"}).body == %{"a" => 1}
+  end
 
-    test "put!/2", c do
-      Bypass.expect(c.bypass, "PUT", "/", fn conn ->
-        conn
-        |> Plug.Conn.put_resp_content_type("application/json")
-        |> Plug.Conn.send_resp(200, Jason.encode_to_iodata!(%{"a" => 1}))
-      end)
+  test "put!/2", c do
+    Bypass.expect(c.bypass, "PUT", "/", fn conn ->
+      conn
+      |> Plug.Conn.put_resp_content_type("application/json")
+      |> Plug.Conn.send_resp(200, Jason.encode_to_iodata!(%{"a" => 1}))
+    end)
 
-      assert Req.put!(c.url, json: %{foo: "bar"}).body == %{"a" => 1}
-    end
+    assert Req.put!(c.url, json: %{foo: "bar"}).body == %{"a" => 1}
+  end
 
-    test "delete!/2", c do
-      Bypass.expect(c.bypass, "DELETE", "/", fn conn ->
-        Plug.Conn.send_resp(conn, 200, "ok")
-      end)
+  test "delete!/2", c do
+    Bypass.expect(c.bypass, "DELETE", "/", fn conn ->
+      Plug.Conn.send_resp(conn, 200, "ok")
+    end)
 
-      assert Req.delete!(c.url).body == "ok"
-    end
+    assert Req.delete!(c.url).body == "ok"
   end
 end
