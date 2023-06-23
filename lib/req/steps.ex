0ce7e1ea3223f33eaf3f16131b8506178155859f
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2023-06-23 14:59:52 +0200

    Fix typo

--- lib/req/steps.ex
+++ lib/req/steps.ex
@@ -818,7 +818,7 @@ defmodule Req.Steps do
   | identity      | Returns data as is                              |
 
   This step updates the following headers to reflect the changes:
-  - `content-legnth` is set to the length of the decompressed body
+  - `content-length` is set to the length of the decompressed body
 
   ## Options
 
