560780a45019c00e2b13f557aa8f7895890ad501
Wojtek Mach <wojtek@wojtekmach.pl>
2023-07-10 10:53:26 +0200

    Require Elixir 1.13

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Req.MixProject do
     [
       app: :req,
       version: @version,
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
