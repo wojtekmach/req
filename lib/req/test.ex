7b4abce94c6bbdbb4c84abd944c6e34aee1dc65c
Wojtek Mach <wojtek@wojtekmach.pl>
2024-02-07 14:02:28 +0100

    Initial `Req.Test`, ref #287

--- lib/req/test.ex
+++ lib/req/test.ex
@@ -1,18 +1,41 @@
 defmodule Req.Test do
   @ownership Req.Ownership
 
+  @doc """
+  Returns the stub created by `stub/2`.
+  """
   def stub(name) do
-    case NimbleOwnership.get_owned(@ownership, self()) do
-      %{^name => %{value: value}} ->
+    case NimbleOwnership.fetch_owner(@ownership, callers(), name) do
+      {:ok, owner} when is_pid(owner) ->
+        %{^name => value} = NimbleOwnership.get_owned(@ownership, owner)
         value
 
-      nil ->
+      :error ->
         raise "cannot find stub #{inspect(name)} in process #{inspect(self())}"
     end
   end
 
+  @doc """
+  Creates a stub with given `name` and `value`.
+
+  This function allows stubbing _any_ value and later access it with `stub/1`. It is safe to use
+  in concurrent tests.
+
+  See [module documentation](`Req.Test`) for more examples.
+
+  ## Examples
+
+      iex> Req.Test.stub(MyStub, :foo)
+      iex> Req.Test.stub(MyStub)
+      :foo
+      iex> Task.async(fn -> Req.Test.stub(MyStub) end) |> Task.await()
+      :foo
+  """
   def stub(name, value) do
-    NimbleOwnership.allow(@ownership, self(), self(), name)
-    NimbleOwnership.get_and_update(@ownership, self(), name, fn _ -> {:ok, %{value: value}} end)
+    NimbleOwnership.get_and_update(@ownership, self(), name, fn _ -> {:ok, value} end)
+  end
+
+  defp callers do
+    [self() | Process.get(:"$callers") || []]
   end
 end
