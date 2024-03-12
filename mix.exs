906b63e201a9dfbe1474a5adf46acec984b0a12f
Andrea Leopardi <an.leopardi@gmail.com>
2024-03-12 15:37:19 +0100

    Relax nimble_owernship requirement (#322)

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule Req.MixProject do
       {:finch, "~> 0.17", finch_opts()},
       {:mime, "~> 1.6 or ~> 2.0"},
       {:jason, "~> 1.0"},
-      {:nimble_ownership, "~> 0.2.0"},
+      {:nimble_ownership, "~> 0.2.0 or ~> 0.3.0"},
       {:nimble_csv, "~> 1.0", optional: true},
       {:aws_signature, "~> 0.3.2", optional: true},
       {:plug, "~> 1.0", [optional: true] ++ plug_opts()},
