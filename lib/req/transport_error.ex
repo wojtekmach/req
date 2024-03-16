e7dd7ebd585de3d617ce73eff39dfcc96af1ca3b
Wojtek Mach <wojtekmach@users.noreply.github.com>
2024-03-16 19:53:33 +0100

    Update lib/req/transport_error.ex
    Co-authored-by: Andrea Leopardi <an.leopardi@gmail.com>

--- lib/req/transport_error.ex
+++ lib/req/transport_error.ex
@@ -2,7 +2,7 @@ defmodule Req.TransportError do
   @moduledoc """
   Represents an error with the transport used by an HTTP connection.
 
-  This is a standardised exception that all Req adapters should use for transport layer related
+  This is a standardised exception that all Req adapters should use for transport-layer-related
   errors.
 
   This exception is based on `Mint.TransportError`.
