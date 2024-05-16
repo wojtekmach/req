0b675bfe5164692b80720636f697d04ef5f7a7f3
Wojtek Mach <wojtek@wojtekmach.pl>
2024-05-16 17:57:25 +0200

    Filter out ezstd from test logs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,19 @@
+defmodule EzstdFilter do
+  # Filter out:
+  # 17:56:39.116 [debug] Loading library: ~c"/path/to/req/_build/test/lib/ezstd/priv/ezstd_nif"
+  def filter(log_event, _opts) do
+    case log_event.msg do
+      {"Loading library" <> _, [path]} ->
+        ^path = to_charlist(Application.app_dir(:ezstd, "priv/ezstd_nif"))
+        :stop
+
+      _ ->
+        :ignore
+    end
+  end
+end
+
+:logger.add_primary_filter(:ezstd_filter, {&EzstdFilter.filter/2, []})
+
 ExUnit.configure(exclude: :integration)
 ExUnit.start()
