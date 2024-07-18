29b1d129c0c7047851d13902c16d3b6462af8c62
Wojtek Mach <wojtekmach@users.noreply.github.com>
2024-07-18 15:46:16 +0200

    Update lib/req/request.ex

--- lib/req/request.ex
+++ lib/req/request.ex
@@ -90,7 +90,7 @@ defmodule Req.Request do
 
         * `collectable` - stream response body into a `t:Collectable.t/0`. For example:
 
-              into: File.stream!(“path”)
+              into: File.stream!("path")
 
     * `:options` - the options to be used by steps. The exact representation of options is private.
       Calling `request.options[key]`, `put_in(request.options[key], value)`, and
