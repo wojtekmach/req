1b17c0c431162239b34c6aad452b3c852a8a513b
Jonatan Kłosko <jonatanklosko@gmail.com>
2024-11-25 18:32:59 +0800

    Add option to configure Finch pool :pool_max_idle_time

--- lib/req/steps.ex
+++ lib/req/steps.ex
@@ -59,6 +59,7 @@ defmodule Req.Steps do
       :receive_timeout,
       :pool_timeout,
       :unix_socket,
+      :pool_max_idle_time,
 
       # TODO: Remove on Req 1.0
       :output,
@@ -749,6 +750,9 @@ defmodule Req.Steps do
     * `:receive_timeout` - socket receive timeout in milliseconds, defaults to `15_000`.
 
     * `:unix_socket` - if set, connect through the given UNIX domain socket.
+
+    * `:pool_max_idle_time` - the maximum number of milliseconds that a pool can be
+      idle before being terminated, used only by HTTP1 pools. Default to `:infinity`.
 
     * `:finch_private` - a map or keyword list of private metadata to add to the Finch request.
       May be useful for adding custom data when handling telemetry with `Finch.Telemetry`.
