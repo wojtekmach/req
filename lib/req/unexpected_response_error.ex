a9811c86fd07f235a2b62feceb2fd858c251d63c
Wojtek Mach <wojtek@wojtekmach.pl>
2025-07-21 16:09:18 +0200

    up

--- lib/req/unexpected_response_error.ex
+++ lib/req/unexpected_response_error.ex
@@ -1,10 +1,20 @@
 defmodule Req.UnexpectedResponseError do
-  defexception [:expected, :response]
+  @moduledoc """
+  An exception returned by `Req.Steps.expect/1` when response has unexpected status.
+
+  The public fields are:
+
+    * `:expected_status` - the expected HTTP response status
+
+    * `:response` - the HTTP response
+  """
+
+  defexception [:expected_status, :response]
 
   @impl true
-  def message(%{expected: expected, response: response}) do
+  def message(%{expected_status: expected_status, response: response}) do
     """
-    expected status #{inspect(expected)}, got: #{response.status}
+    expected status #{inspect(expected_status)}, got: #{response.status}
 
     headers:
     #{inspect(response.headers, pretty: true)}
