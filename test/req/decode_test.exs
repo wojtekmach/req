ca643c5bfb80916e19dd89ea61402c120d926d1a
Wojtek Mach <wojtek@wojtekmach.pl>
2026-08-14 12:35:11 +0200

    Revert "Req.Tar: decode to `%Req.Tar{}`"
    This reverts commit 5df1cccf119a7026729c8dd74634b18c1bd8ec98.

--- test/req/decode_test.exs
+++ test/req/decode_test.exs
@@ -195,27 +195,23 @@ defmodule Req.DecodeTest do
   describe "tar" do
     test "not decoded by default" do
       %{req: req} =
-        serve("GET /": &send_resp_tar(&1, [{"foo.txt", "bar"}]))
+        serve("GET /": &send_resp_tar(&1, [{~c"foo.txt", "bar"}]))
 
       body = Req.get!(req).body
       assert is_binary(body)
     end
 
     test "content-type" do
-      files = [{"foo.txt", "bar"}]
+      files = [{~c"foo.txt", "bar"}]
       %{req: req} = serve("GET /": &send_resp_tar(&1, files))
 
       resp = Req.get!(req, decoders: [:tar])
       assert resp.status == 200
-      assert resp.body == %Req.Tar{files: files}
-      assert resp.body["foo.txt"] == "bar"
-      assert Enum.to_list(resp.body) == files
-      assert inspect(resp.body) == ~s|#Req.Tar<[{"foo.txt", "bar"}]>|
-      assert inspect(resp.body, pretty: true) == ~s|#Req.Tar<[\n  {"foo.txt", "bar"}\n]>|
+      assert resp.body == files
     end
 
     test "path" do
-      files = [{"foo.txt", "bar"}]
+      files = [{~c"foo.txt", "bar"}]
 
       %{req: req, url: url} =
         serve(
@@ -228,11 +224,11 @@ defmodule Req.DecodeTest do
 
       resp = Req.get!(req, url: "#{url}/foo.tar", decoders: [:tar])
       assert resp.status == 200
-      assert resp.body == %Req.Tar{files: files}
+      assert resp.body == files
     end
 
     test "path, content type with charset utf8" do
-      files = [{"foo.txt", "bar"}]
+      files = [{~c"foo.txt", "bar"}]
 
       %{req: req, url: url} =
         serve(
@@ -245,11 +241,11 @@ defmodule Req.DecodeTest do
 
       resp = Req.get!(req, url: "#{url}/foo.tar", decoders: [:tar])
       assert resp.headers["content-type"] == ["application/octet-stream; charset=utf-8"]
-      assert resp.body == %Req.Tar{files: files}
+      assert resp.body == files
     end
 
     test "path, binary/octet-stream content-type" do
-      files = [{"foo.txt", "bar"}]
+      files = [{~c"foo.txt", "bar"}]
 
       %{req: req, url: url} =
         serve(
@@ -262,11 +258,11 @@ defmodule Req.DecodeTest do
 
       resp = Req.get!(req, url: "#{url}/foo.tar", decoders: [:tar])
       assert resp.status == 200
-      assert resp.body == %Req.Tar{files: files}
+      assert resp.body == files
     end
 
     test "path, no content-type" do
-      files = [{"foo.txt", "bar"}]
+      files = [{~c"foo.txt", "bar"}]
 
       %{req: req, url: url} =
         serve(
@@ -277,11 +273,11 @@ defmodule Req.DecodeTest do
 
       resp = Req.get!(req, url: "#{url}/foo.tar.gz", decoders: [:tgz])
       assert resp.status == 200
-      assert resp.body == %Req.Tar{files: files}
+      assert resp.body == files
     end
 
     test "tar.gz (path)" do
-      files = [{"foo.txt", "bar"}]
+      files = [{~c"foo.txt", "bar"}]
 
       %{req: req, url: url} =
         serve(
@@ -294,7 +290,7 @@ defmodule Req.DecodeTest do
 
       resp = Req.get!(req, url: "#{url}/foo.tar.gz", decoders: [:tgz])
       assert resp.status == 200
-      assert resp.body == %Req.Tar{files: files}
+      assert resp.body == files
     end
 
     test "invalid" do
--- test/req/tar_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule Req.TarTest do
-  use Req.Case, async: true
-
-  doctest Req.Tar,
-    tags: [:integration],
-    inspect_opts: [pretty: true, limit: 2]
-end
